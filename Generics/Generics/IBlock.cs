28e1ca729dd59bdc68bf277931b8cf0799dab209
agambh06 <gmbnhmw13@gmail.com>
2025-06-22 19:51:48 +0300

    empty

---
+++ Generics/Generics/IBlock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+
+    public interface IBlock<T> where T : class
+    {
+        T Process(T input);
+    }
+
+}
---
+++ Generics/Generics/UpperCaseBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class UpperCaseBlock<string>:IBlock<string>
+    {
+       public string Process(string input)
+       {
+            return input.ToUpper();
+       }
+    }
+}
